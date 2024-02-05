4ce22b74fb10a3396ad7a27e230f6a623d61d769
颜勄炜 <minwei.yan@eeoa.com>
2024-02-05 23:53:17 +0800

    增加构造函数用例

--- C++Fundamentals/Construct/main.cpp
+++ C++Fundamentals/Construct/main.cpp
@@ -1,39 +1,27 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include "test.h"
 
-class Test {
-public:
-    Test(int a) : m_a(a) {qInfo() << "construct";};
-    Test(const Test& a){
-        qInfo() << "left value copy construct";
-    }
-    Test(Test&& a){
-        qInfo() << "right value copy construct";
-    }
-
-    void operator=(const Test& a) {
-        qInfo() << "left value copy assignment construct";
-    }
-
-    void operator=(Test&& a) {
-        qInfo() << "right value move assignment construct";
-    }
-    void print() const {qInfo() << m_a;}
-private:
-    int m_a = 0;
-};
-
-const Test& createTest() {
-//    auto t = ;
-    return Test(1);
+static Test s_test = Test(0);
+Test getTempTest() {
+    return Test(3);
 }
 
-void copyTest(const Test& t) {
-    auto t2 = t;
+const Test& getLTest() {
+    return s_test;
 }
 
-void copyTest(Test&& t) {
-    auto t2 = t;
+Test&& getRTest() {
+    return Test(2);
+}
+
+template <typename T>
+void setTest(T&& t) {
+    auto t2 = std::forward(t);
+}
+
+void setRTest(Test&& t) {
+    auto t2 = std::move(t);
 }
 /***
 对象不存在，且没用别的对象来初始化，就是调用了构造函数；A a; A a = A(1)
@@ -44,14 +32,32 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     {
-        auto t = createTest();
-        t.print();
+        qInfo() << "111111 return temp value test begin 111111";
+        auto t = getTempTest(); t.print();
+        qInfo() << "******************";
+        const auto& lt = getTempTest(); lt.print();
+        qInfo() << "-----------------";
+        Test&& rt = getTempTest(); rt.print();
+        qInfo() << "111111 return L value test begin 111111";
+        auto t2 = getLTest(); t2.print();
+        qInfo() << "******************";
+        const auto& lt2 = getLTest(); lt2.print();
+        qInfo() << "-----------------";
+        auto&& rt2 = getLTest(); rt2.print();    // 注意这里用auto可以，但用Test就不行
+        qInfo() << "111111 return R value test begin 111111";
+        auto t3 = getRTest(); t3.print();
+        qInfo() << "******************";
+        const auto& lt3 = getRTest(); lt3.print();
+        qInfo() << "-----------------";
+        Test&& rt3 = getRTest(); rt3.print();
     }
-//    auto t = Test(2);
-//    copyTest(t);
-//    copyTest(Test(2));
-//    Test t  = Test(3);
-//    Test t2 = 4;
-//    t2 = std::move(t);
+    {
+        qInfo() << "222222 return temp value test begin 22222";
+        auto t = getTempTest(); t.print();
+        qInfo() << "******************";
+        const auto& lt = getTempTest(); lt.print();
+        qInfo() << "-----------------";
+        auto&& rt = getTempTest(); rt.print();
+    }
     return a.exec();
 }
---
+++ C++Fundamentals/Construct/test.h
@@ -0,0 +1,31 @@
+#ifndef TEST_H
+#define TEST_H
+#include <QDebug>
+
+class Test {
+public:
+    Test(int a) : m_a(a) {qInfo() << "construct";};
+    Test(const Test& a){
+        m_a = a.m_a;
+        qInfo() << "left value copy construct";
+    }
+    Test(Test&& a){
+        m_a = a.m_a;
+        qInfo() << "right value copy construct";
+    }
+
+    void operator=(const Test& a) {
+        m_a = a.m_a;
+        qInfo() << "left value copy assignment construct";
+    }
+
+    void operator=(Test&& a) {
+        m_a = a.m_a;
+        qInfo() << "right value move assignment construct";
+    }
+    void print() const {qInfo() << m_a;}
+private:
+    int m_a = 0;
+};
+
+#endif // TEST_H
